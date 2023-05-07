6e2d13eb2aa6b3f36c371d1e580a9ae7c5387ddc
wilberforce001 <kenmisik@gmail.com>
2023-05-07 11:27:12 +0300

    created function getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hi!")
+
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice(){
+    console.log("Computer");
+    console.log("Wilberforce");
+}
+
+getComputerChoice();
+
