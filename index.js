27603f01e670f1f1816d31e24b77da6284d896aa
wilberforce001 <kenmisik@gmail.com>
2023-05-08 08:57:29 +0300

    Set the functions that returns the result of the game

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ console.log("Hi!")
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
-    const choice = options[(Math.random() * options.length)]
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+
+}
+
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie"
+    }
+    else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ) {
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
 }
 
 getComputerChoice();
