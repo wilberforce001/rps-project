5bd24eee14b173acd8c5bff22c228399d56e6517
wilberforce001 <kenmisik@gmail.com>
2023-05-08 10:21:28 +0300

    used prompt to display a dialog that prompts the user to input some text and changed them to lower case sing toLowerCase

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ function checkWinner(playerSelection, computerSelection){
     }
 }
 
+
+
+
 function playRound(playerSelection, computerSelection){
     const result = checkWinner(playerSelection, computerSelection);
     if(result == "Tie"){
@@ -38,7 +41,29 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function getPlayerChoice() {
+    let validatedInput = false;
+    while(validatedInput == false){
+        const choice = prompt("Rock Paper Scissors");
+        if(choice == null){
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if (options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+
+function game(){
+    console.log("Welcome!")
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game()
 
