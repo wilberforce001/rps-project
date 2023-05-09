514bec71d59fa296268d432c82f58669fe9ddd39
wilberforce001 <kenmisik@gmail.com>
2023-05-09 11:52:26 +0300

    Set the functions that checks how many times one has won

--- revisit-rps/index.js
+++ revisit-rps/index.js
@@ -10,13 +10,15 @@ function startGame() {
     let imgs = document.querySelectorAll('img')
     imgs.forEach((img) =>
     img.addEventListener(('click'), () => {
-        
+        if (img.id) {
+            playRound(img.id);
+        }
     }))
 
     
 }
 
-function playRound(round) {
+function playRound(playerChoice) {
     
     const computerChoice = computerSelect();
    
@@ -30,12 +32,15 @@ function computerSelect(){
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function checkWins() {
+    
+}
+
 function checkWinner(choice1, choice2) {
     if (
         (choice1 == "rock" && choice2 == "scissors") ||
         (choice1 == "scissors" && choice2 == "paper") ||
         (choice1 == "paper" && choice2 == "rock")
     )
-} {
     return "player";
 }
